95af16ebc97a167f2d62f0449a63818764127ad0
cbjorklund88 <clara.bjorklund@hotmail.com>
2018-11-12 10:59:57 +0100

    Start fresh

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,54 +1,12 @@
 import React from "react"
-import Beer from "./beer"
+
 
 class App extends React.Component {
-
-  state = {
-    beers: [],
-    filter: ""
-  }
-
-  componentDidMount() {
-    fetch("http://localhost:8080/beers")
-      .then(response => response.json())
-      .then(json => {
-        this.setState({ beers: json })
-      })
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.filter !== this.state.filter) {
-      //new request
-      fetch(`http://localhost:8080/beers?style=${this.state.filter}`)
-        .then(response => response.json())
-        .then(json => {
-          this.setState({ beers: json })
-        })
-    }
-  }
-
-  handleFilterChange = (event) => {
-    this.setState({
-      filter: event.target.value
-    })
-  }
 
   render() {
     return (
       <div>
-
-        <select onChange={this.handleFilterChange}>
-        <option value="">All</option>
-        <option value="Lager">Lager</option>
-        <option value="IPA">IPA</option>
-        <option value="Pilsner">Pilsner</option>
-        <option value="Wheat">Wheat</option>
-        </select>
-
-        {this.state.beers.map(beer => (
-          <Beer
-            beer={beer}/>
-        ))}
+        <h1>Hej</h1>
       </div>
     )
   }
