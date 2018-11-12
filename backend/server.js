b779a447db94055da5f14eddfeda9f82e25b41e5
cbjorklund88 <clara.bjorklund@hotmail.com>
2018-11-12 11:32:39 +0100

    Built server starter

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,33 @@ const mongoUrl = "mongodb://localhost/project-work"
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
 mongoose.Promise = Promise // Allows mongo to use javascript promises
 
+const Animal = mongoose.model("Animal", {
+  Name: String,
+  Sex: {
+    type: String,
+    required: true
+  },
+  Size: {
+    type: String,
+    required: true
+  },
+  Age: {
+    type: Number,
+    required: true
+  },
+  CatOk: {
+    type: Boolean,
+    required: true
+  },
+  DogOk: {
+    type: Boolean,
+    required: true
+  }
+})
 
+app.get("/", (req, res) => {
+  res.send("Animals API")
+})
 
 app.listen(8080, () => { // .listen is a express funcition
   console.log("Server running!")
