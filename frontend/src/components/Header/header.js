1bc685f49eaa14386e4b434954272b7e0853bc3d
cbjorklund88 <clara.bjorklund@hotmail.com>
2018-11-20 13:19:51 +0100

    hambuerger menu works

--- frontend/src/components/Header/header.js
+++ frontend/src/components/Header/header.js
@@ -17,19 +17,21 @@ export default class Header extends React.Component {
       <div>
         <header className="header">
           <h1>Header</h1>
-          <input
-            id="hamburger"
-            className="hamburger-checkbox"
-            type="checkbox" />
-          <label htmlFor="hamburger" className="hamburger-label">
-            <div className="hamburger-bar1" />
-            <div className="hamburger-bar2" />
-            <div className="hamburger-bar3" />
-          </label>
+            <input
+              id="hamburger"
+              className="hamburger-checkbox"
+              type="checkbox" />
+              <div className="hamburger-container">
+            <label htmlFor="hamburger" className="hamburger-label">
+              <div className="hamburger-bar1" />
+              <div className="hamburger-bar2" />
+              <div className="hamburger-bar3" />
+            </label>
+          </div>
           <nav className="navbar-list">
-            <a class="link-item" href="#"> Om oss </a>
-            <a class="link-item" href="#"> Logga in </a>
-            <a class="link-item" href="#"> Kontakt </a>
+            <a className="link-item" href="#"> Om oss </a>
+            <a className="link-item" href="#"> Logga in </a>
+            <a className="link-item" href="#"> Kontakt </a>
           </nav>
         </header>
       </div>
