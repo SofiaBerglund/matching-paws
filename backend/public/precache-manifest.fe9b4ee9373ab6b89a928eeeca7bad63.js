019c4c6b4fe088a005df8ccb03cf52e219d5085b
Sofia Berglund <berglund.sofia@gmail.com>
2018-11-20 15:14:17 +0100

    public folder created

---
+++ backend/public/precache-manifest.fe9b4ee9373ab6b89a928eeeca7bad63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f19ad623c09243b12387",
+    "url": "/static/js/main.f19ad623.chunk.js"
+  },
+  {
+    "revision": "9635fc798a74674c8d91",
+    "url": "/static/js/1.9635fc79.chunk.js"
+  },
+  {
+    "revision": "f19ad623c09243b12387",
+    "url": "/static/css/main.3ebac02e.chunk.css"
+  },
+  {
+    "revision": "6f674be01ef811af7c8a2fca6cfb8129",
+    "url": "/index.html"
+  }
+];
