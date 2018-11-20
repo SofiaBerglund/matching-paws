83b397cf59c80839d1ed1e49257dcaba522b2cbe
Sofia Berglund <berglund.sofia@gmail.com>
2018-11-20 13:09:50 +0100

    added text: hittar inga matchningar

--- frontend/src/components/Header/header.js
+++ frontend/src/components/Header/header.js
@@ -2,7 +2,6 @@ import React from "react"
 import "./header.scss"
 
 export default class Header extends React.Component {
-
   state = {
     isActive: false
   }
@@ -16,20 +15,30 @@ export default class Header extends React.Component {
     return (
       <div>
         <header className="header">
-          <h1>Header</h1>
+          <h1>Matching Paws</h1>
           <input
             id="hamburger"
             className="hamburger-checkbox"
-            type="checkbox" />
+            type="checkbox"
+          />
           <label htmlFor="hamburger" className="hamburger-label">
             <div className="hamburger-bar1" />
             <div className="hamburger-bar2" />
             <div className="hamburger-bar3" />
           </label>
           <nav className="navbar-list">
-            <a class="link-item" href="#"> Om oss </a>
-            <a class="link-item" href="#"> Logga in </a>
-            <a class="link-item" href="#"> Kontakt </a>
+            <a class="link-item" href="#">
+              {" "}
+              Om oss{" "}
+            </a>
+            <a class="link-item" href="#">
+              {" "}
+              Logga in{" "}
+            </a>
+            <a class="link-item" href="#">
+              {" "}
+              Kontakt{" "}
+            </a>
           </nav>
         </header>
       </div>
