666a3fbf6471a03ac753ba802329dbf54e821e4d
Sofia Berglund <berglund.sofia@gmail.com>
2018-11-21 13:42:58 +0100

    deploy test

---
+++ public/precache-manifest.3ea2c36a937e0f58d0bab1f60bf9714b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2736fb23f92bcf23b501",
+    "url": "/static/js/main.2736fb23.chunk.js"
+  },
+  {
+    "revision": "3b2ec261c4ac334e702b",
+    "url": "/static/js/1.3b2ec261.chunk.js"
+  },
+  {
+    "revision": "2736fb23f92bcf23b501",
+    "url": "/static/css/main.d640ce71.chunk.css"
+  },
+  {
+    "revision": "6d5142385b105e6b8d7d76c65546d406",
+    "url": "/index.html"
+  }
+];
