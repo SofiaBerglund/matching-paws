b49ff317b4c61c724b4360ca73b17b99613484cf
Sofia Berglund <berglund.sofia@gmail.com>
2018-11-22 13:56:07 +0100

    some changes

--- frontend/src/components/ResultPage/result-page.js
+++ frontend/src/components/ResultPage/result-page.js
@@ -1,10 +1,24 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import "./result-page.scss"
+import DetailedInfoPage from "../DetailedInfoPage/detailed-info-page"
 
 export default class ResultPage extends React.Component {
   state = {
-    searchResult: this.props
+    searchResult: this.props,
+    showModal: false
+  }
+
+  showModal = () => {
+    this.setState({
+      showModal: true
+    })
+  }
+
+  hideModal = () => {
+    this.setState({
+      showModal: false
+    })
   }
 
   render() {
@@ -25,8 +39,43 @@ export default class ResultPage extends React.Component {
               <input type="submit" value="Läs mer!" />
             </div>
           </Link>
+
+          {/* <h2>{name}</h2>
+          <div className="image-container">
+            <img src={image} alt="" />
+          </div>
+          <p>Kön: {sex.charAt(0).toUpperCase() + sex.slice(1)}</p>
+          <p>Storlek: {size.charAt(0).toUpperCase() + size.slice(1)}</p>
+          <p>Ålder: {age.charAt(0).toUpperCase() + age.slice(1)}</p>
+          <div className="submit-button">
+            <img id="paw-print" src="./vit-tass.png" alt="" />
+            <input type="submit" value="Läs mer!" />
+          </div>
+          <Modal show={this.state.showModal} handleClose={this.hideModal}>
+            <DetailedInfoPage
+              name={name}
+              image={image}
+              sex={sex}
+              size={size}
+              age={age}
+              id={id}
+            />
+          </Modal> */}
         </div>
       </div>
     )
   }
 }
+
+// const Modal = ({ handleClose, show, children }) => {
+//   return (
+//     <div className={show ? "modal display-block" : "modal display-none"}>
+//       <section className="modal-main">
+//         <button className="close-button" onClick={handleClose}>
+//           &times;
+//         </button>
+//         {children}
+//       </section>
+//     </div>
+//   )
+// }
