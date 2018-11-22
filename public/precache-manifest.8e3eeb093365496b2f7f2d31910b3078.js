38ad1afa0caf4408cf345f95fd0af938d14dbf4c
Sofia Berglund <berglund.sofia@gmail.com>
2018-11-22 17:35:16 +0100

    deploy to heroku

---
+++ public/precache-manifest.8e3eeb093365496b2f7f2d31910b3078.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "562087bf481cd8ab7d2d",
+    "url": "/static/js/main.562087bf.chunk.js"
+  },
+  {
+    "revision": "de8f4597d90cd8b2115b",
+    "url": "/static/js/1.de8f4597.chunk.js"
+  },
+  {
+    "revision": "562087bf481cd8ab7d2d",
+    "url": "/static/css/main.2bd5ce10.chunk.css"
+  },
+  {
+    "revision": "c4dba0f6bfe704216b0d69996666240d",
+    "url": "/index.html"
+  }
+];
